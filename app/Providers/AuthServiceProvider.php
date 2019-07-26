f4dc83eab3bf1467a8b3e95bd2df24a37326fb3a
vagrant <vagrant@homestead>
2019-07-26 05:33:08 +0000

    Commit_1

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace simplePageProject_2\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider{
+
+   /**
+    * The policy mappings for the application.
+    *
+    * @var array
+    */
+   protected $policies = [
+      // 'simplePageProject_2\Model' => 'simplePageProject_2\Policies\ModelPolicy',
+   ];
+
+   /**
+    * Register any authentication / authorization services.
+    *
+    * @return void
+    */
+   public function boot(){
+      $this->registerPolicies();
+   }
+}
